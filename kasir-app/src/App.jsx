6d14138fdd2b0b54321dc878ddc0e97f85dbf217
macbook-pro <macbook-prom4@macbook-pros-MacBook-Pro.local>
2025-02-16 22:03:10 +0700

    add sidebar list

--- kasir-app/src/App.jsx
+++ kasir-app/src/App.jsx
@@ -16,12 +16,13 @@ export default class App extends Component {
   
     this.state = {
        menus:[],
+       categoriYangDipilih: 'Makanan'
     }
   }
 
   componentDidMount(){
     try {
-      axios.get(API_URL + 'products')
+      axios.get(API_URL + 'products?category.nama=' + this.state.categoriYangDipilih)
       .then(res => {
         const menus = res.data;
         this.setState({menus});
@@ -29,10 +30,26 @@ export default class App extends Component {
     } catch(error) {
       console.log(error)
     }
-   }
+   } 
+
+   changeCategory = (value) => {
+    this.setState({
+      categoriYangDipilih: value,
+      menus: []
+    })
+    axios.get(API_URL + 'products?category.nama=' + value)
+    .then(res => {
+      const menus = res.data;
+      this.setState({menus});
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+
+
 
   render() {
-    const {menus} = this.state
+    const {menus,categoriYangDipilih} = this.state
     return (
       <div>
         <Navbarcomponent />
@@ -40,7 +57,7 @@ export default class App extends Component {
           <Container fluid>
             <Row>
               <Col md={4}>
-                <List />
+                <List changeCategory={this.changeCategory} categoriYangDipilih={categoriYangDipilih} />
               </Col>
               <Col md={4}>
                 <h4>Daftar Produk</h4>
--- kasir-app/src/components/List.jsx
+++ kasir-app/src/components/List.jsx
@@ -1,14 +1,69 @@
 import React, { Component } from "react";
-import Col from "react-bootstrap/esm/Col";
+import { Col, Row, Tab, ListGroup } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "../utils/constant";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils, faCoffee, faCheese } from "@fortawesome/free-solid-svg-icons";
+import "../styling/list.css"; 
+
+const Icon = ({ nama }) => {
+  if (nama === "Makanan")
+    return <FontAwesomeIcon icon={faUtensils} className="icon" />;
+  if (nama === "Minuman")
+    return <FontAwesomeIcon icon={faCoffee} className="icon" />;
+  if (nama === "Cemilan")
+    return <FontAwesomeIcon icon={faCheese} className="icon" />;
+  return <FontAwesomeIcon icon={faUtensils} className="icon" />;
+};
 
 export default class List extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    try {
+      axios.get(API_URL + "categories").then((res) => {
+        const categories = res.data;
+        this.setState({ categories });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
+    const { categories } = this.state;
+    console.log(categories);
     return (
       <div>
-        <Col md={3} mt="2" />
         <h4>Kategori</h4>
         <hr />
-        <Col />
+        <Tab.Container id="list-group-tabs-example">
+          <Row>
+            <Col sm={12}>
+              <ListGroup>
+                {categories &&
+                  categories.map((category) => (
+                    <ListGroup.Item
+                      action
+                      href={`#${category.id}`}
+                      key={category.id}
+                      className="d-flex align-items-center"
+                      onClick={() => this.props.changeCategory(category.nama)}
+                    >
+                      <span className="category-name">{category.nama}</span>
+                      <Icon nama={category.nama} />
+                    </ListGroup.Item>
+                  ))}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Tab.Container>
       </div>
     );
   }
